20860084bee3cc16ad7ec5adbb7bc9f72e1f9ea3
ZAINKHAN25 <zainarfeen87@gmail.com>
2023-09-18 20:05:08 +0500

    selected ko background dediya

--- src/mainquiz.js
+++ src/mainquiz.js
@@ -5,22 +5,22 @@ function Mainquiz({ selectedquestionsarea }) {
   const [indexofquestion, setindexofquestion] = useState(0);
   const [selectedAnswers, setSelectedAnswers] = useState([]);
   const [isSubmitted, setIsSubmitted] = useState(false); // Track if the quiz is submitted
-  const [answerSelected, setAnswerSelected] = useState(false); // Track if an answer is selected
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(-1); // Track the selected answer index
 
   const handleNextQuestion = () => {
     if (indexofquestion < selectedquestionsarea.data.length - 1) {
       setindexofquestion((prevIndex) => prevIndex + 1);
-      setAnswerSelected(false); // Reset answerSelected when moving to the next question
+      setSelectedAnswerIndex(-1); // Reset selectedAnswerIndex when moving to the next question
     } else {
       // Set isSubmitted to true when all questions are answered
       setIsSubmitted(true);
     }
   };
 
-  const handleAnswerSelect = (selectedAnswer) => {
+  const handleAnswerSelect = (selectedAnswer, index) => {
     selectedAnswers[indexofquestion] = selectedAnswer;
     setSelectedAnswers([...selectedAnswers]);
-    setAnswerSelected(true); // Set answerSelected to true when an answer is selected
+    setSelectedAnswerIndex(index); // Set the selected answer index
   };
 
   // Helper function to calculate the score
@@ -47,7 +47,7 @@ function Mainquiz({ selectedquestionsarea }) {
   const question = selectedquestionsarea.data[indexofquestion];
 
   return (
-    <div className="container mainquizpage flex-column align-items-start">
+    <div className="container mainquizpage flex-column align-items-start pt-2">
       <h1 className="mb-5 fs-1">Question Number {indexofquestion + 1}:</h1>
       <p className="fs-5 mt-1">{question.question}</p>
       <ol className="d-flex olofoptions flex-column align-items-start">
@@ -56,19 +56,25 @@ function Mainquiz({ selectedquestionsarea }) {
             <label className="my-1">
               <input
                 type="radio"
-                style={{visibility: "hidden"}}
+                style={{ visibility: "hidden" }}
                 name={`question-${indexofquestion}`}
                 value={answer}
-                onChange={() => handleAnswerSelect(answer)}
+                onChange={() => handleAnswerSelect(answer, index)}
                 checked={selectedAnswers[indexofquestion] === answer}
               />
-              <li className="ps-5 answerli py-2"> {answer}</li>
+              <li
+                className={`ps-5 answerli py-2 ${
+                  index === selectedAnswerIndex ? "backgroundtrue" : ""
+                }`}
+              >
+                {answer}
+              </li>
             </label>
           </div>
         ))}
       </ol>
       <div>
-        {answerSelected && ( // Only display the button if an answer is selected
+        {selectedAnswerIndex !== -1 && ( // Only display the button if an answer is selected
           <button
             className="btn my-3  mx-2 btnbackgroundGreen fs-5"
             onClick={handleNextQuestion}
